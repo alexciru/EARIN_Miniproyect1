0fc034556e660448f4471d59ba4fde1bc7411d2a
alexciru <alejandro.cirugeda@estudiante.uam.es>
2019-03-23 17:02:53 +0100

    test

--- test.py
+++ test.py
@@ -18,16 +18,27 @@ and run Python code; now you just need to learn Python!
 """
 
 import sys
-
+global name = None
 # Define a main() function that prints a little greeting.
 def main():
+
   # Get the name from the command line, using 'World' as a fallback.
   if len(sys.argv) >= 2:
     name = sys.argv[1]
   else:
     name = 'World'
+    function()
   print ('Hello', name)
+
+
+def function():
+  print("hayyyyyyy")
+  name = 'pepe'
+  return
+
 
 # This is the standard boilerplate that calls the main() function.
 if __name__ == '__main__':
   main()
+
+
