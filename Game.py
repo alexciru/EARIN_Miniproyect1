fa798ccd7b48b8eac54497ec8de4855833b7e431
Juancar97 <48989127+Juancar97@users.noreply.github.com>
2019-03-29 16:40:02 +0100

    test 3

--- Game.py
+++ Game.py
@@ -17,7 +17,6 @@ class Game:
         # this function should check if there is the same number of 0 and 1 in a row and see if
         # there is no more that two same digits togueter
         #me come la polla
-        int = 0
         return
 
     def check_collum(self, solution):
